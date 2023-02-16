25591bd2432b327471a87d4408b140a4859c3b3f
Srajan Rai <srajanrai2.0@gmail.com>
2023-02-17 00:04:28 +0530

    Create 2.py

---
+++ Practice/2.py
@@ -0,0 +1,4 @@
+list_is = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dictionary_is = {"John" : 1, "Jane" : 2, "Jack" : 3}
+tuple_is = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+# list is mutable, dictionary is mutable, tuple is immutable
