60bdac1434c3783977ca36f7984c69b3fc4b9134
Srajan Rai <srajanrai2.0@gmail.com>
2023-02-17 00:04:32 +0530

    Create list.py

---
+++ Practice/list.py
@@ -0,0 +1,23 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list1)
+
+# list1.append(11)
+# print(list1)
+
+# list1.insert(0, 0)
+# print(list1)
+
+# list1.remove(5)
+# print(list1)
+# # print(help(list1.remove))
+
+# list2 = ["hello", "world"]
+# print(list2[0][1])
+# print(list2[-1][-1])
+
+# cool_list = ['H', 'e', 'l', 'l', 'o']
+# cool_string = str.join("", cool_list)
+# print(cool_string)
+
+for num in list1:
+    print(num)
